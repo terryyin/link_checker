ca69b475cc672ed59ad473f205b1a31125a82e61
Terry Yin <terry@odd-e.com>
2016-11-09 15:22:57 +0800

    linkchecker

--- linkchecker.py
+++ linkchecker.py
@@ -5,7 +5,7 @@ from scrapy.commands import runspider
 import optparse
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-
+import re
 
 class STDErrorWriter():
     return_code = 0
@@ -15,8 +15,9 @@ class STDErrorWriter():
 
 class LinkCheckerParser(scrapy.Spider):
     name = 'linkchecker'
-    start_urls = ['https://less.works/']
-    #start_urls = ['http://localhost:3000/less/framework/']
+    max_pages = 20
+    #start_urls = ['https://less.works/']
+    start_urls = ['http://localhost:3000/']
     custom_settings = {
         'HTTPERROR_ALLOW_ALL': True
     }
@@ -29,12 +30,34 @@ class LinkCheckerParser(scrapy.Spider):
         if response.status // 100 != 2:
             self.error_writer("{response.url}, status: {response.status}, parent: {parent}".format(response=response, parent=parent))
         else:
-            for quote in response.css('a'):
-                uri = quote.css('a::attr("href")').extract_first()
-                next_page = response.urljoin(uri)
-                if self.start_urls[0] in next_page:
-                    #self.error_writer(next_page)
-                    yield scrapy.Request(next_page, callback=self.parse)
+            if response.request.method == "HEAD" and 'text' in self._content_type(response):
+                yield scrapy.Request(response.url, method="GET", callback=lambda r: self.parse(r, parent))
+        for uri in list(self._css_links(response)) + list(self._html_links(response)):
+            next_page = response.urljoin(uri)
+            if self.start_urls[0] in next_page:
+                if (self.max_pages > 0):
+                    self.max_pages -= 1
+                    yield scrapy.Request(next_page, method="HEAD", callback=lambda r: self.parse(r, response.url))
+
+    def _css_links(self, response):
+        if 'text/css' not in self._content_type(response):
+            return
+        for link in self._find_all_links_in_css(response.text):
+            yield link
+
+    def _html_links(self, response):
+        for uri in response.css('::attr("href")') + response.css('::attr("src")'):
+            yield uri.extract()
+        for style in response.css('style'):
+            for link in self._find_all_links_in_css(style.extract()):
+                yield link
+
+    def _find_all_links_in_css(self, content):
+        for m in re.finditer(r"url\(\s*.(.*).\s*\)", content):
+            yield m.group(1)
+
+    def _content_type(self, response):
+        return ''.join(str(x) for x in response.headers.getlist("content-type"))
 
 def execute(argv=None, settings=None):
     if settings is None:
--- tests/test_linkchecker.py
+++ tests/test_linkchecker.py
@@ -14,7 +14,11 @@ class ErrorWriterForTest(object):
     def __call__(self, text):
         self.cache.append(text)
 
-test_domain = "http://domain"
+class MyRequest:
+    def __init__(self):
+        self.method = 'GET'
+
+test_domain = "http://domain/"
 LinkCheckerParser.start_urls = [test_domain]
 
 @pytest.fixture
@@ -22,42 +26,134 @@ def spider():
     writer = ErrorWriterForTest()
     return LinkCheckerParser(writer)
 
-@pytest.fixture
-def resp_builder():
-    class ResponseBuilder:
-        def __init__(self):
-            self.body = ""
-            self.status_code = 200
+class TextResponseBuilder:
+    def __init__(self):
+        self.body_str = ""
+        self.filename_str = ""
+        self.status_code = 200
+        self.request = MyRequest()
 
-        def status(self, code):
-            self.status_code = code
-            return self
+    def filename(self, fn):
+        self.filename_str = fn
+        return self
+
+    def method(self, m):
+        self.request.method = m
+        return self
+
+    def status(self, code):
+        self.status_code = code
+        return self
+
+    def body(self, text):
+        self.body_str += text
+        return self
+
+    def build(self, **kwargs):
+        return scrapy.http.TextResponse(test_domain + self.filename_str, status=self.status_code, encoding="utf-8", body=self.body_str, request=self.request,**kwargs)
+
+@pytest.fixture
+def html_resp_builder():
+    class HtmlResponseBuilder(TextResponseBuilder):
+        def __init__(self):
+            self.head_str = ""
+            super(HtmlResponseBuilder, self).__init__()
 
         def link(self, text, link):
-            self.body += "<a href='{}'>{}</a>".format(link, text)
+            self.body_str += "<a href='{}'>{}</a>".format(link, text)
+            return self
+
+        def head(self, code):
+            self.head_str += code
             return self
 
         def build(self):
-            return scrapy.http.HtmlResponse(test_domain, status=self.status_code, encoding="utf-8", body="<html><body>"+self.body+"</html>")
-    return ResponseBuilder()
+            return scrapy.http.HtmlResponse(test_domain, status=self.status_code, encoding="utf-8", body="<html><head>"+self.head_str+"</head><body>"+self.body_str+"</html>", headers={'Content-Type': 'text/html; charset=utf-8'}, request=self.request)
+
+    return HtmlResponseBuilder()
+
+@pytest.fixture
+def css_builder():
+    class CssResponseBuilder(TextResponseBuilder):
+        def __init__(self):
+            super(CssResponseBuilder, self).__init__()
+            self.filename_str = "a.css"
+
+        def build(self, **kwargs):
+            return super(CssResponseBuilder, self).build(headers={'Content-Type': 'text/css; charset=utf-8'})
+    return CssResponseBuilder()
+
+@pytest.fixture
+def image_resp_builder():
+    class ImageResponseBuilder(TextResponseBuilder):
+        def __init__(self):
+            super(ImageResponseBuilder, self).__init__()
+            self.filename_str = "a.png"
+
+        def build(self, **kwargs):
+            return super(ImageResponseBuilder, self).build(headers={'Content-Type': 'image/png; charset=utf-8'})
+    return ImageResponseBuilder()
 
 
+def test_head_response_of_html_page(spider, html_resp_builder):
+    requests = [_ for _ in spider.parse(html_resp_builder.method("HEAD").build())]
+    assert requests[0].method == "GET"
 
-def test_no_links_200(spider, resp_builder):
-    requests = [_ for _ in spider.parse(resp_builder.build())]
+def test_head_response_of_image(spider, image_resp_builder):
+    requests = [_ for _ in spider.parse(image_resp_builder.method("HEAD").build())]
+    assert len(requests) == 0
+
+def test_no_links_200(spider, html_resp_builder):
+    requests = [_ for _ in spider.parse(html_resp_builder.build())]
     assert len(spider.error_writer.cache) == 0
     assert len(requests) == 0
 
-def test_404(spider, resp_builder):
-    [_ for _ in spider.parse(resp_builder.status(404).build(), parent="parent")]
+def test_404(spider, html_resp_builder):
+    [_ for _ in spider.parse(html_resp_builder.status(404).build(), parent="parent")]
     assert len(spider.error_writer.cache) == 1
-    assert spider.error_writer.cache[0] == "http://domain, status: 404, parent: parent"
+    assert spider.error_writer.cache[0] == "http://domain/, status: 404, parent: parent"
 
-def test_with_one_link(spider, resp_builder):
-    requests = [_ for _ in spider.parse(resp_builder.link("a", "xxx").build())]
+def test_with_one_link(spider, html_resp_builder):
+    requests = [_ for _ in spider.parse(html_resp_builder.link("a", "xxx.html").build())]
+    assert len(requests) == 1
+    assert requests[0].method == "HEAD"
+
+def test_with_one_external_link(spider, html_resp_builder):
+    requests = [_ for _ in spider.parse(html_resp_builder.link("a", "http://google.com").build())]
+    assert len(requests) == 0
+
+def test_with_image(spider, html_resp_builder):
+    requests = [_ for _ in spider.parse(html_resp_builder.body("<img src='xxx.jpg'/>").build())]
+    assert len(requests) == 1
+    assert requests[0].method == "HEAD"
+
+def test_with_link_in_head(spider, html_resp_builder):
+    requests = [_ for _ in spider.parse(html_resp_builder.head("<link href='xxx.ico'>").build())]
     assert len(requests) == 1
 
-def test_with_one_external_link(spider, resp_builder):
-    requests = [_ for _ in spider.parse(resp_builder.link("a", "http://google.com").build())]
+def test_a_css_file(spider, css_builder):
+    response = css_builder.body(".x{background:url('x.jpg')};").build()
+    requests = [_ for _ in spider.parse(response)]
+    assert len(requests) == 1
+    assert requests[0].url == test_domain + 'x.jpg'
+
+def test_a_different_css_file(spider, css_builder):
+    response = css_builder.body(r'.x{background:url(  "x.jpg"  )};').build()
+    requests = [_ for _ in spider.parse(response)]
+    assert len(requests) == 1
+
+def test_a_html_looks_like_css(spider, html_resp_builder):
+    response = html_resp_builder.body(r'.x{background:url("x.jpg")};').build()
+    requests = [_ for _ in spider.parse(response)]
     assert len(requests) == 0
+
+def test_a_html_with_embedded_css(spider, html_resp_builder):
+    response = html_resp_builder.body(r'<style>.x{background:url("x.jpg")};</style>').build()
+    requests = [_ for _ in spider.parse(response)]
+    assert len(requests) == 1
+
+# no downloading images
+# redirect
+# external
+
 
