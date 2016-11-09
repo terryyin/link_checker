eef00411aad52bb929f1b2755987b63334a4c870
Terry Yin <terry@odd-e.com>
2016-11-09 11:22:30 +0800

    more linkchecer error code

--- linkchecker.py
+++ linkchecker.py
@@ -1,18 +1,27 @@
 from __future__ import print_function
 import sys
 import scrapy
+from scrapy.commands import runspider
+import optparse
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
 
-def stderr_writer(text):
-    print( text, file=sys.stderr)
+
+class STDErrorWriter():
+    return_code = 0
+    def __call__(self, text):
+        print( text, file=sys.stderr)
+        STDErrorWriter.return_code = 2
 
 class LinkCheckerParser(scrapy.Spider):
-    name = 'blogspider'
+    name = 'linkchecker'
     start_urls = ['https://less.works/']
+    #start_urls = ['http://localhost:3000/less/framework/']
     custom_settings = {
         'HTTPERROR_ALLOW_ALL': True
     }
 
-    def __init__(self, error_writer=stderr_writer):
+    def __init__(self, error_writer=STDErrorWriter()):
         super(LinkCheckerParser, self).__init__()
         self.error_writer = error_writer
 
@@ -26,3 +35,25 @@ class LinkCheckerParser(scrapy.Spider):
                 if self.start_urls[0] in next_page:
                     #self.error_writer(next_page)
                     yield scrapy.Request(next_page, callback=self.parse)
+
+def execute(argv=None, settings=None):
+    if settings is None:
+        settings = get_project_settings()
+
+    parser = optparse.OptionParser(formatter=optparse.TitledHelpFormatter(), \
+        conflict_handler='resolve')
+    cmd = runspider.Command()
+    settings.setdict(cmd.default_settings, priority='command')
+    cmd.settings = settings
+    cmd.add_options(parser)
+    opts, args = parser.parse_args(args=argv[2:])
+    cmd.process_options(args, opts)
+
+    crawler_process = CrawlerProcess(settings)
+    crawler_process.crawl(LinkCheckerParser)
+    crawler_process.start()
+    return STDErrorWriter.return_code
+
+if __name__ == '__main__':
+    sys.exit(execute(['scrapy', 'runspider', '--nolog']))
+
